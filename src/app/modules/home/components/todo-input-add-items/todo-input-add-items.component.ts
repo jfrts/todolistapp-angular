4771db1c22410a57af309ca030da83b734beb360
Júlio Freitas <jcsr.frts@gmail.com>
2023-07-19 17:51:14 -0300

    components and layout style

---
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-items',
+  templateUrl: './todo-input-add-items.component.html',
+})
+export class TodoInputAddItemsComponent {
+
+}
---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+})
+export class TodoListComponent {
+
+}

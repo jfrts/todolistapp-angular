a83d60190b4f0530a55fd8b4ffd0f3e524593025
Júlio Freitas <jcsr.frts@gmail.com>
2023-07-20 14:21:54 -0300

    delete tasks

--- src/app/modules/home/components/todo-button-delete-all/todo-button-delete-all.component.ts
+++ src/app/modules/home/components/todo-button-delete-all/todo-button-delete-all.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
+import { Task } from '../../model/task';
 
 @Component({
-  selector: 'app-todo-button-delete-all',
-  templateUrl: './todo-button-delete-all.component.html',
+    selector: 'app-todo-button-delete-all',
+    templateUrl: './todo-button-delete-all.component.html',
 })
 export class TodoButtonDeleteAllComponent {
-
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { Task } from '../../model/task';
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
+    selector: 'app-todo-list',
+    templateUrl: './todo-list.component.html',
 })
 export class TodoListComponent {
+    public tasks: Task[];
+    public openDialog: boolean;
 
+    constructor() {
+        this.openDialog = false;
+        this.tasks = [];
+        this.tasks.push({ title: "Minha Tarefa 1", completed: false });
+        this.tasks.push({ title: "Minha Tarefa 2", completed: true });
+        this.tasks.push({ title: "Minha Tarefa 3", completed: false });
+    }
+
+    public toggleTaskStatus(index: number): void {
+        this.tasks[index].completed = !this.tasks[index].completed; 
+    }
+
+    public deleteTask(index: number): void {
+        this.tasks.splice(index, 1);
+    }
+
+    public toggleDeleteModal(): void {
+        this.openDialog = !this.openDialog;
+    }
+
+    public deleteAllTasks(confirm: boolean): void {
+        this.toggleDeleteModal();
+        if (confirm) {
+            this.tasks = [];
+        }
+    }
 }

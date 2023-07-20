bea17e0df322ce66e3d343ad74412a874cff4866
Júlio Freitas <jcsr.frts@gmail.com>
2023-07-20 18:52:34 -0300

    completed

--- src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-items',
   templateUrl: './todo-input-add-items.component.html',
 })
 export class TodoInputAddItemsComponent {
+    @Output()
+    public emitTaskEvent: EventEmitter<string>;
+    public taskToEmit: string;
 
+    constructor() {
+        this.emitTaskEvent = new EventEmitter();
+        this.taskToEmit = "";
+    }
+
+    public emitTask(task: string) {
+        this.emitTaskEvent.emit(this.taskToEmit);
+        this.taskToEmit = "";
+    }
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,32 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { Task } from '../../model/task';
 
 @Component({
     selector: 'app-todo-list',
     templateUrl: './todo-list.component.html',
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck {
     public tasks: Task[];
     public openDialog: boolean;
 
     constructor() {
         this.openDialog = false;
-        this.tasks = [];
-        this.tasks.push({ title: "Minha Tarefa 1", completed: false });
-        this.tasks.push({ title: "Minha Tarefa 2", completed: true });
-        this.tasks.push({ title: "Minha Tarefa 3", completed: false });
+        this.tasks = this.getStoredTasks();
+    }
+
+    public ngDoCheck(): void {
+        this.tasks.sort(function (first, last) {
+            return Number(first.completed) - Number(last.completed);
+        });
     }
 
     public toggleTaskStatus(index: number): void {
-        this.tasks[index].completed = !this.tasks[index].completed; 
+        this.tasks[index].completed = !this.tasks[index].completed;
+        this.storeTasks(this.tasks);
+    }
+
+    public addTask(inputTitle: string): void {
+        const title = inputTitle.trim();
+        if (title !== "") {
+            const task: Task = {
+                title,
+                completed: false
+            }
+            this.tasks.push(task);
+            this.storeTasks(this.tasks);
+        }
+    }
+    
+    public updateTask(index: number, task: string) {
+        this.tasks[index].title = task;
+        this.storeTasks(this.tasks);
     }
 
     public deleteTask(index: number): void {
         this.tasks.splice(index, 1);
-    }
-
-    public toggleDeleteModal(): void {
-        this.openDialog = !this.openDialog;
+        this.storeTasks(this.tasks);
     }
 
     public deleteAllTasks(confirm: boolean): void {
@@ -34,5 +52,26 @@ export class TodoListComponent {
         if (confirm) {
             this.tasks = [];
         }
+        this.unstoreTasks();
+    }
+
+    private getStoredTasks(): Task[] {
+        const tasks = window.localStorage.getItem("todo-app-tasks");
+        if (!tasks) {
+            return [];
+        }
+        return JSON.parse(tasks);
+    }
+
+    private storeTasks(tasks: Task[]): void {
+        window.localStorage.setItem("todo-app-tasks", JSON.stringify(tasks));
+    }
+
+    private unstoreTasks(): void {
+        window.localStorage.removeItem("todo-app-tasks");
+    }
+
+    public toggleDeleteModal(): void {
+        this.openDialog = !this.openDialog;
     }
 }
